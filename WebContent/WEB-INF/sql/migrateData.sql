bd343a37e6d8794394476f80aa93571cf7156a5c
ldornin <ldornin@gmail.com>
2013-10-28 22:31:00 -0400

    Actually, this should be dataKey because its a kv store.

--- WebContent/WEB-INF/sql/migrateData.sql
+++ WebContent/WEB-INF/sql/migrateData.sql
@@ -2,8 +2,8 @@ USE mysql;
 CREATE DATABASE IF NOT EXISTS migrate DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
 USE migrate;
 CREATE TABLE IF NOT EXISTS `kv_table` (`namespace` varchar(255) NOT NULL DEFAULT '',
-	`wd_data_id` varchar(255) NOT NULL DEFAULT '',
+	`dataKey` varchar(255) NOT NULL DEFAULT '',
 	`className` varchar(255) DEFAULT NULL,
 	`value` mediumblob, `updateTime` bigint(20) DEFAULT NULL,
-	`version` bigint(20) DEFAULT NULL, PRIMARY KEY (`namespace`, `wd_data_id`), INDEX (updateTime))
+	`version` bigint(20) DEFAULT NULL, PRIMARY KEY (`namespace`, `dataKey`), INDEX (updateTime))
 	ENGINE=InnoDB DEFAULT CHARSET=utf8;
